2360c009484ec774d429ea1ca42646814e7df080
hida <minuskg123@gmail.com>
2020-12-05 11:09:04 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,13 @@ function scrollTop() {
 }
 
 window.addEventListener('scroll', scrollTop)
+
+// gsap
+
+gsap.from('.home__img', { opacity: 0, duraion: 2, delay: 0.5, x: 60 });
+gsap.from('.home__data', { opacity: 0, duraion: 2, delay: 0.8, y: 25 })
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', { opacity: 0, duraion: 2, delay: 1, y: 25, ease: 'expo.out', stagger: 0.2 })
+
+gsap.from('.nav__logo, .nav__toggle', { opacity: 0, duraion: 2, delay: 1.5, y: 25, ease: 'expo.out', stagger: 0.2 })
+gsap.from('.nav__item', { opacity: 0, duraion: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: 0.2 })
+gsap.from('.home__social-icon', { opacity: 0, duraion: 2, delay: 2.3, y: 25, ease: 'expo.out', stagger: 0.2 })
